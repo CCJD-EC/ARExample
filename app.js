383e76d9540cf621e7dc750b815c4c8a4bcea4bf
Christian Carrera <jchristiandias@gmail.com>
2023-10-15 22:59:37 -0500

    CCA Correcciones

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use('/', express.static(path.join(__dirname,'/public')));
+app.use('/', express.static(path.join(__dirname,'/docs')));
 
 app.use((req, res) =>{
   res.status(404);
